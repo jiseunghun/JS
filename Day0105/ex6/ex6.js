feb3014d0e5868ceae22a0d7a12efdc8d782b816
Sunhye Moon <ss333567@gmail.com>
2022-01-06 00:30:51 +0900

    study0105-bigdataclass
    study0105

--- Day0105/ex6/ex6.js
+++ Day0105/ex6/ex6.js
@@ -1,5 +1,8 @@
 const btn = document.querySelectorAll(".buttonList_chain button");
 const imgBring = document.querySelector("img");
+const infobtn = document.querySelectorAll(".infoBox_btn button");
+const infobox = document.querySelector(".infoBox_info");
+const getStyle = window.getComputedStyle(imgBring);
 
 const nameTest = [
   "hwangjeongeum",
@@ -14,6 +17,17 @@ const allEventHandle = {
   imgChange: function (i) {
     imgBring.setAttribute("src", `../../img0103/연예인사진/${nameTest[i]}.jpg`);
   },
+  imginfo: function (i) {
+    infobox.innerText = `이미지 정보 출력
+    src : ${imgBring.getAttribute("src")}
+    width : ${imgBring.getAttribute("width")}
+    height : ${imgBring.getAttribute("height")}
+    이미지 스타일 정보 출력 
+    width : ${getStyle.width}
+    height : ${getStyle.height}
+    border-Style : ${getStyle.border}
+    OriginphotoForm : ${getStyle.transformOrigin}`;
+  },
 };
 
 btn[0].addEventListener("click", () => allEventHandle.imgChange(0));
@@ -22,3 +36,5 @@ btn[2].addEventListener("click", () => allEventHandle.imgChange(2));
 btn[3].addEventListener("click", () => allEventHandle.imgChange(3));
 btn[4].addEventListener("click", () => allEventHandle.imgChange(4));
 btn[5].addEventListener("click", () => allEventHandle.imgChange(5));
+
+infobtn[0].addEventListener("click", () => allEventHandle.imginfo(0));
